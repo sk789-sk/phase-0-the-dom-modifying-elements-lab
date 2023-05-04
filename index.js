1d963f02be8bfd2ef71eb3120017976c5cc023da
Shams <skm478@gmail.com>
2023-05-03 20:30:27 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainnode = document.getElementById("main")
+mainnode.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Bob is the champion'
